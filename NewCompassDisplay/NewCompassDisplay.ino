20a635efb55ddf01f0694d7152521d988745544b
blackbicbiro <blackbicbiro@gmail.com>
2016-07-08 15:38:45 +0100

    Menu added to LCD plus 3 buttons. Calbration menus added (cal not working yet)

--- NewCompassDisplay/NewCompassDisplay.ino
+++ NewCompassDisplay/NewCompassDisplay.ino
@@ -3,18 +3,30 @@
 #include <SPI.h>         // needed for Arduino versions later than 0018
 #include <Ethernet.h>
 #include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
+#include <Bounce2.h>
+#define DEBOUNCE 50  // button debouncer, how many ms to debounce, 5+ ms is usually plenty
+
 
 //pins
 SoftwareSerial mySerial(3,2);    // pin 3 = RX  FOR LCD
-int BtnPlusYes = A0;
-int BtnMinusNo =  A1;
-int BtnOk  = A2;
+#define BtnPlusYes A0
+#define BtnMinusNo A1
+#define BtnOk A2
+int BtnPlusYesSTATE = 0;
+int BtnMinusNoSTATE = 0;
+int BtnOkSTATE = 0;
+
 //pin 10  - ethernet sheild
 //pin 11  - ethernet sheild
 //pin 12  - ethernet sheild
 //pin 13  - ethernet sheild
 //pin 14  - ethernet sheild
 
+
+//set up bounce instances for buttons
+Bounce debouncer1 = Bounce();   //BtnPlus
+Bounce debouncer2 = Bounce();   //BtnMinusNo
+Bounce debouncer3 = Bounce();   //BtnOK
 
 // compass varible
 String bearing = 0;          
@@ -61,6 +73,20 @@ void setup() {
   //clear UDP buffer on boot
   ClearPacketBuffer();
 
+  pinMode(BtnPlusYes, INPUT_PULLUP);
+  debouncer1.attach(BtnPlusYes);
+  debouncer1.interval(DEBOUNCE); // interval in ms
+
+  pinMode(BtnMinusNo, INPUT_PULLUP);
+  debouncer2.attach(BtnMinusNo);
+  debouncer2.interval(DEBOUNCE); // interval in ms
+
+  pinMode(BtnOk, INPUT_PULLUP);
+  debouncer3.attach(BtnOk);
+  debouncer3.interval(DEBOUNCE); // interval in ms
+
+
+
   //Start up display message
   mySerial.write(254); // move cursor to beginning of first line
   mySerial.write(128);
@@ -70,10 +96,7 @@ void setup() {
   mySerial.write(128);
   mySerial.write("Compass Unit");
   delay(1000);
-  mySerial.write(254); // move cursor to beginning of first line
-  mySerial.write(128);
-  mySerial.write("                "); // clear display
-  mySerial.write("                ");
+  ClearScreen();
 }
 //////////////////////////// END /////////////////////////////////
 
@@ -86,6 +109,10 @@ void loop() {
   String RXData = ReadPacket();    //Check to see if there is packet infomation. Pass back True/False
   GetValFromPacket(RXData);        //Splits RX'd string into each varbile
   ClearPacketBuffer();             //Clear Buffer so new packets dont get curupted
+
+  ButtonCheckUpdate();    // Check state of buttons and save value
+  CheckMenuPressed();     // check to see if it should go to menu setup
+
   if(CheckStatus() == true){       //Check to make sure Packets are not timing out/head unit missing and display appropitioine info on lcd
     PrintValuesToScreen();         //Display Values to display 
     Serial.println(bearing);
@@ -218,12 +245,14 @@ void ClearPacketBuffer(){
 }
 
 
+
+
+
+
+///////////////////// Print Error message to screen //////////////////////////////////////////
 void PrintErrorToScreen(){
   if(lastScreen != "PrintErrorToScreen"){
-    mySerial.write(254); // move cursor to beginning of first line
-    mySerial.write(128);
-    mySerial.write("                ");
-    mySerial.write("                ");
+    ClearScreen();
     mySerial.write(254); // move cursor to beginning of first line
     mySerial.write(128);
     mySerial.write("     ERROR!");
@@ -241,13 +270,11 @@ void PrintErrorToScreen(){
 
 
 
+///////////////////// Print Compass values to screen ////////////////////////////
 void PrintValuesToScreen(){
   //check to see if this is the first time this is being called apon. if so clear screen and display headers
   if(lastScreen != "PrintValuesToScreen"){
-    mySerial.write(254); // move cursor to beginning of first line
-    mySerial.write(128);
-    mySerial.print("                ");
-    mySerial.print("                ");
+    ClearScreen();
 
     mySerial.write(254); // move cursor to beginning of first line
     mySerial.write(128);
@@ -306,6 +333,208 @@ void PrintValuesToScreen(){
   Oldroll = roll;
   Oldcal = cal;
 }
+
+
+
+
+
+
+//////////////////// clear screen ////////////////////////////////
+void ClearScreen(){
+  mySerial.write(254); // move cursor to beginning of first line
+  mySerial.write(128);
+  mySerial.print("                ");
+  mySerial.print("                ");
+}
+
+
+
+
+
+////////////////////// Menu Pressed ///////////////////////////////
+void CheckMenuPressed(){
+  if(BtnOkSTATE == LOW){
+    while(true){
+      ButtonCheckUpdate();    // Check state of buttons and save value
+      if(BtnOkSTATE == HIGH){
+        break;
+      }
+    }
+    int Menuloop = true;
+    int MenuSelect = 1;
+    ClearScreen();
+    while(Menuloop == true){
+      //Dispay and select menu;
+      switch(MenuSelect){
+      case 1:
+        if(lastScreen != "bearingAdj"){
+          ClearScreen();
+          mySerial.write(254); // move cursor to beginning of first line
+          mySerial.write(128);
+          mySerial.print("Bearing        +");
+          mySerial.print("Adjust         -");
+          lastScreen = "bearingAdj";
+        }
+        break;
+      case 2:
+        if(lastScreen != "TiltAdj"){
+          ClearScreen();
+          mySerial.write(254); // move cursor to beginning of first line
+          mySerial.write(128);
+          mySerial.print("Tilt           +");
+          mySerial.print("Adjust         -");
+          lastScreen = "TiltAdj";
+        }
+        break;
+      case 3:
+        if(lastScreen != "rollAdj"){
+          ClearScreen();
+          mySerial.write(254); // move cursor to beginning of first line
+          mySerial.write(128);
+          mySerial.print("Roll           +");
+          mySerial.print("Adjust         -");
+          lastScreen = "rollAdj";
+        }  
+        break;
+      case 4:
+        if(lastScreen != "EXIT"){
+          ClearScreen();
+          mySerial.write(254); // move cursor to beginning of first line
+          mySerial.write(128);
+          mySerial.print("EXIT           +");
+          mySerial.print("               -");
+          lastScreen = "EXIT";
+        }        
+        break;     
+      } 
+
+
+
+      // check for plus button being pressed once
+      ButtonCheckUpdate();    // Check state of buttons and save value
+      if(BtnPlusYesSTATE == LOW){
+        while(true){
+          ButtonCheckUpdate();    // Check state of buttons and save value
+          if(BtnPlusYesSTATE == HIGH){
+            break;
+          }
+        }
+
+        MenuSelect = ++MenuSelect;
+        if(MenuSelect > 4){
+          MenuSelect = 1;
+        }
+      }
+
+
+      // check for minus button being pressed once
+      if(BtnMinusNoSTATE == LOW){
+        while(true){
+          ButtonCheckUpdate();    // Check state of buttons and save value
+          if(BtnMinusNoSTATE == HIGH){
+            break;
+          }
+        }
+
+        MenuSelect = --MenuSelect;
+        if(MenuSelect < 1){
+          MenuSelect = 4;
+        }
+      }  
+      Serial.print("am i gettings here");
+      //Action Selected menu
+      if(BtnOkSTATE == LOW){
+        Serial.print("got here");
+        while(true){
+          ButtonCheckUpdate();    // Check state of buttons and save value
+          if(BtnOkSTATE == HIGH){
+            break;
+          }
+        }
+        switch(MenuSelect){
+        case 1:
+
+
+          break; 
+
+        case 2:
+
+          break;
+
+        case 3:
+
+          break;
+
+        case 4:
+          Menuloop = false;
+          break;   
+        }
+      }
+
+
+    }  // end while loop        
+  }//end if loop
+  ClearPacketBuffer();
+}
+
+
+
+
+
+
+
+//////////////// calibrate bearing and send update to head //////////////////////////
+void BearingCal(){
+  while(true){
+
+       ButtonCheckUpdate();
+       if(lastScreen != "BearingCal"){
+          ClearScreen();
+          mySerial.write(254); // move cursor to beginning of first line
+          mySerial.write(128);
+          mySerial.print("Bearing =      +");
+          mySerial.print("Cal Val =      -");
+          lastScreen = "BearingCal";
+        }// end of if loop
+       if(Oldbearing != bearing){
+          mySerial.write(254); // move cursor to beginning of first line
+          mySerial.write(137);
+          mySerial.write("     ");
+          mySerial.write(254); // move cursor to beginning of first line
+          mySerial.write(137);
+          mySerial.print(bearing);
+          mySerial.print((char)223);
+        }
+        
+  Oldbearing = bearing;      
+  String RXData = ReadPacket();    //Check to see if there is packet infomation. Pass back True/False
+  GetValFromPacket(RXData);        //Splits RX'd string into each varbile    
+}//end of while loop
+}
+
+
+
+
+
+
+
+
+////////////////////// Check and update button state ////////////////////////
+void ButtonCheckUpdate(){
+  debouncer1.update();
+  debouncer2.update();
+  debouncer3.update();
+  BtnPlusYesSTATE = debouncer1.read();
+  BtnMinusNoSTATE = debouncer2.read();
+  BtnOkSTATE = debouncer3.read();
+
+}
+
+
+
+
+
+
 
 
 
